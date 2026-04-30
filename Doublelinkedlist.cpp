dfcda0643b91acfb52d29b11f9f94c454b2d45b0
aswarina <lyxsyyawa@gmail.com>
2026-04-30 13:20:28 +0700

    implementasi fungsi pencarian data (search)

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -135,6 +135,24 @@ public:
             cout << "\nList is empty" << endl; 
             return;
         }
+
+        //step  1: mark first node as currenNode
+        Node *currentNode = START;
+
+        //step 2: repeat until currentNode == NULL
+        cout << "\nRecords in ascending order of roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            //step 3: move to next node
+            currentNode = currentNode->next;
+            i++;
+        }
     }
+
+    
 }
 
