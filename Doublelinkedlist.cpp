bd2a3ff9b3b2e1ec031afc94358f3d4e4751ee49
aswarina <lyxsyyawa@gmail.com>
2026-04-30 12:24:18 +0700

    inisialisasi pointer head dan tail

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -23,6 +23,16 @@ public:
     }
 
     void addNode()
-}
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student : ";
+        cin >> nim;
 
+        //step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        //step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+    }
+}
 
