b1b9bd2662d5f3114a5ab85fb6c8cd22fd9f26d6
aswarina <lyxsyyawa@gmail.com>
2026-04-30 12:39:57 +0700

    implementasi fungsi tambah di akhir (insetlast)

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -56,6 +56,20 @@ public:
             START = newNode;
             return;
         }
+
+        // insert in between node
+        // step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if(current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDupliccate roll numbers not allowed" << endl;
+            return;
+        }
     }
 }
 
