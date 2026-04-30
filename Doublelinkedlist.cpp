d263744b23cdfe9c4d6798ef2df6885204b3d1d6
aswarina <lyxsyyawa@gmail.com>
2026-04-30 12:17:02 +0700

    inisialisasi library dan setup namespace

---
+++ Doublelinkedlist.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoubleLinkedList
+{
+    
+}
+
+
